1618127238c56d6227115d4fc201afe0e3aca7bf
Kaung Hein Htet <52240936+KHH-AKA-Lucifer@users.noreply.github.com>
2024-08-05 14:44:25 +0630

    fix: add comments

--- super_project/sql_app/database.py
+++ super_project/sql_app/database.py
@@ -1,13 +1,21 @@
+# create engine
 from sqlalchemy import create_engine
+# create a base
 from sqlalchemy.ext.declarative import declarative_base
+# for database session
 from sqlalchemy.orm import sessionmaker
 
+# create a database link
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
+# create an engine
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread" : False}
 )
 
+# create a session
 SessionLocal = sessionmaker(autocommit= False, autoflush=False, bind=engine)
 
+# create a base class
+# we will use this class later to construct tables
 Base = declarative_base()
